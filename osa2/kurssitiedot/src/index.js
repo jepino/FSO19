497304cd45a188f4d3f8c89e9cdb2c687b601fe6
Jerri Pinomäki <jerri.pinomaki@gmail.com>
2019-08-31 09:57:48 +0300

    kurssitiedot step6

---
+++ osa2/kurssitiedot/src/index.js
@@ -0,0 +1,62 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header name={course.name} />
+      <Content parts={course.parts} />
+    </div>
+  );
+};
+
+const Header = ({ name }) => {
+  return (
+    <div>
+      <h1>{name}</h1>
+    </div>
+  );
+};
+
+const Content = ({ parts }) => {
+  return (
+    <div>
+      <Parts parts={parts} />
+    </div>
+  );
+};
+
+const Parts = ({ parts }) =>
+  parts.map(part => (
+    <p>
+      {part.name} {part.exercises}
+    </p>
+  ));
+
+const App = () => {
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7
+      },
+      {
+        name: "State of a component",
+        exercises: 14
+      }
+    ]
+  };
+
+  return (
+    <div>
+      <Course course={course} />
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
