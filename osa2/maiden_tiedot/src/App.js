c2da0d81268b8b66ef44aa934220876d42247fc9
Jerri Pinomäki <jerri.pinomaki@gmail.com>
2019-09-03 14:20:35 +0300

    2.12

---
+++ osa2/maiden_tiedot/src/App.js
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Filter from "./components/Filter";
+import Results from "./components/Results";
+
+const App = () => {
+  const [filter, setFilter] = useState("");
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    console.log("effect");
+    axios
+      .get(
+        "https://restcountries.eu/rest/v2/all?fields=name;capital;population;languages;flag"
+      )
+      .then(response => {
+        console.log("promise fulfilled");
+        setCountries(response.data);
+      });
+  }, []);
+
+  const handleFilterChange = event => {
+    setFilter(event.target.value);
+  };
+
+  return (
+    <div>
+      <Filter value={filter} onChange={handleFilterChange} />
+      <Results results={countries} filter={filter} />
+    </div>
+  );
+};
+
+export default App;
---
+++ osa2/maiden_tiedot/src/components/Results.js
@@ -0,0 +1,50 @@
+import React from "react";
+
+const Results = ({ results, filter }) => {
+  const fResults = results.filter(result =>
+    result.name.toLowerCase().includes(filter.toLowerCase())
+  );
+
+  if (fResults.length === 0) {
+    return <div>no matches</div>;
+  } else if (fResults.length === 1) {
+    return (
+      <div>
+        <h1>{fResults[0].name}</h1>
+        <p>capital {fResults[0].capital}</p>
+        <p>population {fResults[0].population}</p>
+        <h2>languages</h2>
+        <ul>
+          <Languages languages={fResults[0].languages} />
+        </ul>
+        <img
+          src={fResults[0].flag}
+          alt={`flag of ${fResults[0].name}`}
+          width="30%"
+        />
+      </div>
+    );
+  } else if (fResults.length < 10) {
+    return (
+      <div>
+        {fResults.map(c => (
+          <div key={c.name}>{c.name}</div>
+        ))}
+      </div>
+    );
+  } else {
+    return <div>Too many matches, specify another filter</div>;
+  }
+};
+
+const Languages = languages => {
+  const lNames = languages.languages.map(l => <li key={l.name}>{l.name}</li>);
+
+  return (
+    <div>
+      <ul>{lNames}</ul>
+    </div>
+  );
+};
+
+export default Results;
