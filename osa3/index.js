5e390b2fa863b78abcf2464495b4a69c8edded8f
Jerri Pinomäki <jepino@utu.fi>
2019-11-05 20:01:00 +0200

    3.5

--- osa3/index.js
+++ osa3/index.js
@@ -49,11 +49,24 @@ app.get("/api/persons/:id", (request, response) => {
   }
 });
 
+const generateId = () => {
+  return Math.floor(Math.random() * 1000000);
+};
+
 app.delete("/api/persons/:id", (request, response) => {
   const id = Number(request.params.id);
   persons = persons.filter(person => person.id !== id);
 
   response.status(204).end();
+});
+
+app.post("/api/persons", (request, response) => {
+  const person = request.body;
+  person.id = generateId();
+
+  persons = persons.concat(person);
+
+  response.json(person);
 });
 
 const PORT = 3001;
